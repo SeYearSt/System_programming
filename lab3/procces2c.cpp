81ce537b2c15d5f6cb8e6d6be52586018b7ab0fe
SeYearSt <jtiks06@gmail.com>
2018-05-08 14:55:59 +0300

    finished task2

--- lab3/procces2c.cpp
+++ lab3/procces2c.cpp
@@ -1,48 +1,204 @@
 #include <windows.h> 
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <conio.h>
 #include <tchar.h>
-#define szBuf 512
 
-int main(void)
-{
-	HANDLE hPipe;
-	DWORD dwWritten;
-	char message[szBuf];
+#define szBuffer 255
 
-	hPipe = CreateFile(TEXT("\\\\.\\pipe\\Pipe"),
-		GENERIC_READ | GENERIC_WRITE,
-		0,
-		NULL,
-		OPEN_EXISTING,
-		0,
-		NULL);
+int main() {
 
-	if (hPipe != INVALID_HANDLE_VALUE)
-	{
-		bool exit = false;
-		while (!exit) {
-		
-			printf("Enter message: ");
+	char ReadBuffer[szBuffer] = { 0 };
+	char WriteBuffer[szBuffer] = "Default message";
 
-			fgets(message, szBuf,stdin);
+	DWORD dwBytesToWrite;
+	DWORD dwBytesWritten = 0;
 
-			WriteFile(hPipe,
-				message,
-				strlen(message) + 1,
-				&dwWritten,
-				NULL);
-			
-			if (message[0] == '!') {
-				exit = true;
+	DWORD dwBytesRead;
+
+	HANDLE hNamedPipe;
+
+	if (!WaitNamedPipe("\\\\.\\pipe\\my_pipe", NMPWAIT_USE_DEFAULT_WAIT)) {
+		std::cout << "Server is unaccessible.\n";
+		_getch();
+		return -1;
+	}
+
+	hNamedPipe = CreateFile("\\\\.\\pipe\\my_pipe", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+
+	if (hNamedPipe == INVALID_HANDLE_VALUE) {
+		std::cout << "Problem with create pipe\n";
+		std::cout << "Error: " << GetLastError();
+		_getch();
+		return -1;
+	}
+
+	while (true) {
+
+			std::cout << "Client> ";
+			std::cin.getline(WriteBuffer, szBuffer);
+
+			if (strcmp(WriteBuffer,"Close connection") == 0) {
+				break;
 			}
-		}
-		CloseHandle(hPipe);
-	}
-	else {
-		printf("absent pipe!\n");
-	}
 
+			dwBytesToWrite = (DWORD)strlen(WriteBuffer);
+
+			if (!WriteFile(hNamedPipe, WriteBuffer, dwBytesToWrite, &dwBytesWritten, NULL)) {
+				std::cout << "Fail while writing to pipe\n";
+				std::cout << "Error: " << GetLastError();
+			}
+
+			if (strcmp(WriteBuffer, "Server close") == 0) {
+				break;
+			}
+
+			ReadFile(hNamedPipe, ReadBuffer, szBuffer - 1, &dwBytesRead, NULL);
+
+			ReadBuffer[dwBytesRead] = '\0';
+			std::cout << "Server> " << ReadBuffer << "\n";
+		}
+
+	CloseHandle(hNamedPipe);
+
+	std::cout << "Press any key to close application\n";
 	_getch();
-	return (0);
+	return 0;
 }
+
+
+//int main(int argc, char *argv[])
+//{
+//	if (argc < 2) {
+//		printf("Incorrect count of arguments\n");
+//		_getch();
+//		return 0;
+//	}
+//
+//
+//	HANDLE hPipe;
+//	char buffer[szBuffer];
+//	DWORD dwRead;
+//
+//
+//	std::string strPipename("\\\\.\\pipe\\");
+//	strPipename += argv[1];
+//
+//	hPipe = CreateNamedPipe(TEXT(strPipename.c_str()), PIPE_ACCESS_DUPLEX,
+//		PIPE_TYPE_MESSAGE,  
+//		1,
+//		szBuffer * 16,
+//		szBuffer * 16,
+//		NMPWAIT_USE_DEFAULT_WAIT,
+//		NULL);
+//
+//	while (hPipe != INVALID_HANDLE_VALUE)
+//	{
+//		static int pr = 0;
+//		if (!pr) {
+//			printf("Server started!\n");
+//			printf("Waiting for client...!\n");
+//		}
+//		pr = 1;
+//			if (ConnectNamedPipe(hPipe, NULL) != FALSE)   // wait for someone to connect to the pipe
+//			{
+//				std::cout << "Smb connected.\n";
+//
+//				while (ReadFile(hPipe, buffer, szBuffer - 1, &dwRead, NULL) != FALSE)
+//				{
+//					buffer[dwRead] = '\0';
+//
+//					std::cout << buffer << "\n";
+//
+//					if (buffer[dwRead - 1] == '%') {
+//						break;
+//					}
+//				}
+//			}
+//
+//		printf("Close pipe\n");
+//		DisconnectNamedPipe(hPipe);
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#include <windows.h> 
+//#include <string>
+//#include <iostream>
+//#include <stdio.h>
+//#include <conio.h>
+//#include <tchar.h>
+//
+//#define BUFSIZE 512
+//
+//int _tmain(int argc, TCHAR *argv[])
+//{
+//	HANDLE hPipe;
+//	TCHAR  chBuf[BUFSIZE];
+//
+//	std::string strPipename = "\\\\.\\pipe\\";
+//	std::string message;
+//
+//	if (argc < 3) {
+//		printf("Incorrect count of arguments\n");
+//		_getch();
+//		return 0;
+//	}
+//
+//	strPipename += argv[1]; //open pip by this name 
+//
+//	message = argv[2];  // save message to send
+//
+//	// Try to open a named pipe; wait for it, if necessary. s
+//	LPCSTR lpszPipename = TEXT(strPipename.c_str());
+//	LPCSTR lpvMessage = TEXT(message.c_str());
+//
+//	DWORD dwWritten;
+//
+//
+//	hPipe = CreateFile(TEXT(strPipename.c_str()),
+//			GENERIC_READ | GENERIC_WRITE,
+//			0,
+//			NULL,
+//			OPEN_EXISTING,
+//			0,
+//			NULL);
+//		if (hPipe != INVALID_HANDLE_VALUE)
+//		{
+//			WriteFile(hPipe,
+//				message.c_str(),
+//				message.length(),  
+//				&dwWritten,
+//				NULL);
+//
+//			CloseHandle(hPipe);
+//		}
+//
+//		return (0);
+//	}
--- lab3/procces2s.cpp
+++ lab3/procces2s.cpp
@@ -1,49 +1,153 @@
 #include <windows.h> 
-#include <stdio.h>
+#include <iostream>
+#include <string>
 #include <conio.h>
 #include <tchar.h>
-#define szBuf 1024
 
-int main(void)
-{
-	HANDLE hPipe;
-	char buffer[1024];
-	DWORD dwRead;
+#define szBuffer 255
 
+int main() {
 
-	hPipe = CreateNamedPipe(TEXT("\\\\.\\pipe\\Pipe"),
-		PIPE_ACCESS_DUPLEX,
-		PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,  
-		1,
-		szBuf * 16,
-		szBuf * 16,
-		NMPWAIT_USE_DEFAULT_WAIT,
-		NULL);
+	char ReadBuffer[szBuffer] = { 0 };
+	char WriteBuffer[szBuffer] = "Default message";
 
-	while (hPipe != INVALID_HANDLE_VALUE)
-	{
-		static int pr = 0;
-		if (!pr) {
-			printf("Server started!\n");
-			printf("Wait fo Message!\n");
-		}
-		pr = 1;
-		if (ConnectNamedPipe(hPipe, NULL) != FALSE)   // wait for someone to connect to the pipe
-		{
-			while (ReadFile(hPipe, buffer, sizeof(buffer) - 1, &dwRead, NULL) != FALSE)
+	DWORD dwBytesToWrite;
+	DWORD dwBytesWritten = 0;
+
+	DWORD dwBytesRead;
+
+	HANDLE hNamedPipe = CreateNamedPipe(
+		TEXT("\\\\.\\pipe\\my_pipe"),  // назва каналу 
+		PIPE_ACCESS_DUPLEX,  // відкриття в дуплексному режиму (READONLY | WRITEONLY)
+		PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE,  // передавання повідмолень як повідомлення а не побайтно
+		PIPE_UNLIMITED_INSTANCES, // система сама визначає скільки можна створити екзмеплярів залежно від ресурсів
+		szBuffer, // розмір буферу виходу	
+		szBuffer, // розмір буферу входу
+		NMPWAIT_USE_DEFAULT_WAIT, // дефолтне чекання
+		NULL); // атрибути безпеки
+
+	if (hNamedPipe == INVALID_HANDLE_VALUE) {
+		std::cout << "Problem with create pipe\n";
+		std::cout << "Error: " << GetLastError();
+		_getch();
+		return -1;
+	}
+
+	bool exit = false;
+
+	int clientCount = 0;
+
+	std::cout << "Server started\n";
+
+	while (!exit) {
+
+		std::cout << "Wait for client...\n";
+
+		ConnectNamedPipe(hNamedPipe, NULL);
+
+		std::cout << "Client #" << ++clientCount << " connected\n";
+
+		while (ReadFile(hNamedPipe, ReadBuffer, szBuffer - 1, &dwBytesRead, NULL)) {
+
+			if (dwBytesRead > 0 && dwBytesRead <= szBuffer - 1)
 			{
-				buffer[dwRead] = '\0';
+				ReadBuffer[dwBytesRead] = '\0'; 
 
-				printf("%s", buffer);
+				std::cout << "Client> "<<ReadBuffer << "\n";
 
-				if (buffer[0] == '!') {
+				if (strcmp(ReadBuffer, "Server close") == 0) {
+					exit = true;
 					break;
 				}
 			}
+
+			std::cout << "Server> ";
+			std::cin.getline(WriteBuffer, szBuffer);
+
+			/*if (strcmp(WriteBuffer, "Exit chat") == 0) {
+				DisconnectNamedPipe(hNamedPipe);
+			}
+
+
+			if (strcmp(WriteBuffer, "Close server") == 0) {
+				DisconnectNamedPipe(hNamedPipe);
+				CloseHandle(hNamedPipe);
+				return 1;
+			}*/
+
+
+			dwBytesToWrite = (DWORD)strlen(WriteBuffer);
+
+			if (!WriteFile(hNamedPipe, WriteBuffer, dwBytesToWrite, &dwBytesWritten, NULL)) {
+				std::cout << "Fail while writing to pipe\n";
+				std::cout << "Error: " << GetLastError();
+			}
 		}
-		printf("Close pipe\n");
-		DisconnectNamedPipe(hPipe);
+
+		DisconnectNamedPipe(hNamedPipe);
+		std::cout << "Connection closed.\n";
 	}
 
+	CloseHandle(hNamedPipe);
+
+	std::cout << "Press any key to close application\n";
+	_getch();
 	return 0;
 }
+
+
+//int main(int argc, char *argv[])
+//{
+//	if (argc < 2) {
+//		printf("Incorrect count of arguments\n");
+//		_getch();
+//		return 0;
+//	}
+//
+//
+//	HANDLE hPipe;
+//	char buffer[szBuffer];
+//	DWORD dwRead;
+//
+//
+//	std::string strPipename("\\\\.\\pipe\\");
+//	strPipename += argv[1];
+//
+//	hPipe = CreateNamedPipe(TEXT(strPipename.c_str()), PIPE_ACCESS_DUPLEX,
+//		PIPE_TYPE_MESSAGE,  
+//		1,
+//		szBuffer * 16,
+//		szBuffer * 16,
+//		NMPWAIT_USE_DEFAULT_WAIT,
+//		NULL);
+//
+//	while (hPipe != INVALID_HANDLE_VALUE)
+//	{
+//		static int pr = 0;
+//		if (!pr) {
+//			printf("Server started!\n");
+//			printf("Waiting for client...!\n");
+//		}
+//		pr = 1;
+//			if (ConnectNamedPipe(hPipe, NULL) != FALSE)   // wait for someone to connect to the pipe
+//			{
+//				std::cout << "Smb connected.\n";
+//
+//				while (ReadFile(hPipe, buffer, szBuffer - 1, &dwRead, NULL) != FALSE)
+//				{
+//					buffer[dwRead] = '\0';
+//
+//					std::cout << buffer << "\n";
+//
+//					if (buffer[dwRead - 1] == '%') {
+//						break;
+//					}
+//				}
+//			}
+//
+//		printf("Close pipe\n");
+//		DisconnectNamedPipe(hPipe);
+//	}
+//
+//	return 0;
+//}
